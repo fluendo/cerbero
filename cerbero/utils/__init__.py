cbca8a4f8cd2d3696c4805495526d0a33b59261c
Andoni Morales Alastruey <ylatuya@gmail.com>
2014-10-22 12:56:07 +0200

    Add support for desktop windows 8

--- cerbero/utils/__init__.py
+++ cerbero/utils/__init__.py
@@ -193,6 +193,7 @@ def system_info():
         dmap = {'xp': DistroVersion.WINDOWS_XP,
                 'vista': DistroVersion.WINDOWS_VISTA,
                 '7': DistroVersion.WINDOWS_7,
+                '8': DistroVersion.WINDOWS_8,
                 'post2008Server': DistroVersion.WINDOWS_8}
         if win32_ver in dmap:
             distro_version = dmap[win32_ver]
