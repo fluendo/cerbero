342bd76bf1955803525980898f495ed3910ae53c
Victor Guzman <vguzman@fluendo.com>
2015-06-16 12:53:13 +0200

    Support Ubuntu 15.04 Vivid Vervet

--- cerbero/utils/__init__.py
+++ cerbero/utils/__init__.py
@@ -150,6 +150,8 @@ def system_info():
                 distro_version = DistroVersion.UBUNTU_TRUSTY
             elif d[2] == 'utopic':
                 distro_version = DistroVersion.UBUNTU_UTOPIC
+            elif d[2] == 'vivid':
+                distro_version = DistroVersion.UBUNTU_VIVID
             elif d[1].startswith('6.'):
                 distro_version = DistroVersion.DEBIAN_SQUEEZE
             elif d[1].startswith('7.') or d[1].startswith('wheezy'):
