347d2d3f2d29fb0956ff8a051d56593cc09484ad
Victor Martos <vmartos@fluendo.com>
2016-01-12 13:19:33 +0100

    Add support for Windows 10

--- cerbero/utils/__init__.py
+++ cerbero/utils/__init__.py
@@ -200,6 +200,7 @@ def system_info():
                 'vista': DistroVersion.WINDOWS_VISTA,
                 '7': DistroVersion.WINDOWS_7,
                 '8': DistroVersion.WINDOWS_8,
+                '10': DistroVersion.WINDOWS_10,
                 'post2008Server': DistroVersion.WINDOWS_8}
         if win32_ver in dmap:
             distro_version = dmap[win32_ver]
