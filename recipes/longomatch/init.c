f40040e13430529bdc40b78c9512f1bdb8aa4bc0
Andoni Morales Alastruey <ylatuya@gmail.com>
2016-08-13 21:17:33 +0200

    Use mono-addins from nuget packages

--- recipes/longomatch/init.c
+++ recipes/longomatch/init.c
@@ -4,7 +4,7 @@
 static void custom_init ()
 {
   gchar *win_path, *exe_path, *mono_path, *db40_path, *lgm_path, *lib_path;
-  gchar *addins_path, *mono_lib_path, *mono_lib_facades_path, *etc_path;
+  gchar *mono_lib_path, *mono_lib_facades_path, *etc_path;
   gchar *gtk_path, *gst_path;
 
   exe_path = g_win32_get_package_installation_directory_of_module (NULL);
@@ -17,15 +17,13 @@ static void custom_init ()
       "gtk-sharp-2.0", NULL);
   db40_path = g_build_filename (exe_path, "lib", "cli", "Db4objects.Db4o-8.0",
       NULL);
-  addins_path = g_build_filename (exe_path, "lib", "mono",
-      "mono-addins", NULL);
   lgm_path = g_build_filename (exe_path, "lib", "longomatch", NULL);
   gst_path = g_build_filename (exe_path, "lib", "longomatch", "plugins", "gstreamer-0.10", NULL);
   win_path = g_strdup_printf ("%s;%s", g_getenv ("PATH"), gst_path);
 
   g_setenv ("PATH", win_path, TRUE);
-  mono_path = g_strdup_printf("%s;%s;%s;%s;%s;%s", mono_lib_path,
-      mono_lib_facades_path, gtk_path, db40_path, addins_path, lgm_path);
+  mono_path = g_strdup_printf("%s;%s;%s;%s;%s", mono_lib_path,
+      mono_lib_facades_path, gtk_path, db40_path, lgm_path);
   g_setenv ("MONO_PATH", mono_path, TRUE);
   mono_set_dirs (lib_path, etc_path);
   g_print ("Using PATH %s\n", win_path);
