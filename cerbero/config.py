8cb4968e7e1e85e75e0117bc6e10e5a589250cc2
Xavi Artigas <xartigas@fluendo.com>
2016-09-12 16:36:43 +0200

    Add binary_commits property
    These are the SVN revision numbers of the already-built binary codecs

--- cerbero/config.py
+++ cerbero/config.py
@@ -89,7 +89,7 @@ class Config (object):
                    'build_tools_prefix', 'build_tools_sources',
                    'build_tools_cache', 'home_dir', 'recipes_commits',
                    'ios_platform', 'extra_build_tools', 'target_arch_flags',
-                   'extra_bootstrap_packages']
+                   'extra_bootstrap_packages', 'binary_commits']
 
     def __init__(self):
         self._check_uninstalled()
@@ -304,6 +304,7 @@ class Config (object):
         self.set_property('build_tools_sources', None)
         self.set_property('build_tools_cache', None)
         self.set_property('recipes_commits', {})
+        self.set_property('binary_commits', {})
         self.set_property('extra_build_tools', {})
         self.set_property('extra_bootstrap_packages', {})
 
