72cc8faf9406dd4cbd0ccbbfe82007bf8ce0424e
Andoni Morales Alastruey <ylatuya@gmail.com>
2016-09-21 20:25:48 +0200

    Add support for macOS Sierra

--- cerbero/enums.py
+++ cerbero/enums.py
@@ -82,6 +82,7 @@ class DistroVersion:
     OS_X_LEOPARD = 'osx_leopard'
     OS_X_YOSEMITE = 'osx_yosemite'
     OS_X_EL_CAPITAN = 'osx_el_capitan'
+    OS_X_SIERRA = 'osx_sierra'
     IOS_6_0 = 'ios_6_0'
     IOS_6_1 = 'ios_6_1'
     IOS_7_0 = 'ios_7_0'
