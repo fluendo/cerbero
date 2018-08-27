15fff06d8bde5cd311aef8bff735424348a2f3fa
Stephane Cerveau <scerveau@fluendo.com>
2018-08-27 11:56:21 +0200

    OSXRelocator: Fix constructor call

--- cerbero/tools/osxuniversalgenerator.py
+++ cerbero/tools/osxuniversalgenerator.py
@@ -98,7 +98,7 @@ class OSXUniversalGenerator(object):
             tmp_inputs.append(tmp)
             shutil.copy(f, tmp.name)
             prefix_to_replace = [d for d in dirs if d in f][0]
-            relocator = OSXRelocator (self.output_root, prefix_to_replace, self.output_root,
+            relocator = OSXRelocator (self.output_root, prefix_to_replace,
                                       False)
             # since we are using a temporary file, we must force the library id
             # name to real one and not based on the filename
