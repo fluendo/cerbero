fluendo/cerbero

commit 382273e1f655c80df6263bb3899324659ded2eab

Consider only new files for installed_files
This ensures no previous files are kept in the cache.
This may happen if a recipe changes the name of any
of its artifacts without cleaning the old one.

cerbero/build/cookbook.py
@@ -197,13 +197,21 @@ class CookBook (object):
         @type files: list
         '''
         status = self._recipe_status(recipe_name)
-        installed_files = list(set(files + status.installed_files))
-        existing_files = list(filter(lambda x: os.path.exists(os.path.join(self._config.prefix, x)), installed_files))
-        removed_files = list(set(installed_files) - set(existing_files))
-        if removed_files:
+        installed_files = set(files)
+        previous_files = set(list(filter(lambda x: os.path.exists(os.path.join(self._config.prefix, x)), status.installed_files)))
+        existing_files = set(filter(lambda x: os.path.exists(os.path.join(self._config.prefix, x)), installed_files))
+        non_existing_files = list(installed_files - existing_files)
+        if non_existing_files:
             m.warning('There are some installed files for recipe %s that don\'t exist anymore: %s\n'
-                      'Removing them from recipe\'s cache' % (recipe_name, removed_files))
-        status.installed_files = existing_files
+                      'Removing them from recipe\'s cache' % (recipe_name, non_existing_files))
+        remove_files = list(previous_files - existing_files)
+        remove_files = [os.path.join(self._config.prefix, f) for f in remove_files]
+        if remove_files:
+            m.message('Removing old files that existed in previous installation but don\'t exist '
+                      'anymore:\n{}'.format(remove_files))
+            for f in remove_files:
+                os.remove(f)
+        status.installed_files = list(existing_files)
         self._update_status(recipe_name, status)
         return status.installed_files
 
@@ -265,7 +273,11 @@ class CookBook (object):
         @type recipe_name: str
         '''
         if recipe_name in self.status:
+            # We need to save the previous installed_files to remove the
+            # old files that may not be present in a new installation
+            installed_files = self.status[recipe_name].installed_files
             del self.status[recipe_name]
+            self._recipe_status(recipe_name).installed_files = installed_files
             self.save()
 
     def recipe_needs_build(self, recipe_name):

commit 0a9bc42219a2dc6b43a70fb74a6db0d278b878ef

Fix recipes reset when not needed
Commit 382273e1f655c80df6263bb3899324659ded2eab introduced a
regression because `installed_files` is kept to be able to clean
old files when installing the new version of a recipe.
However, this made the recipes to be kept in the status, which
caused too many resets when unnecessary.

cerbero/build/cookbook.py
@@ -344,7 +344,7 @@ class CookBook (object):
         @rtype: list
         '''
         recipe = self.get_recipe(recipe_name)
-        return [r for r in list(self.recipes.values()) if recipe.name in r.deps]
+        return list(set([r for r in list(self.recipes.values()) if recipe.name in r.deps]))
 
     def save(self):
         try:
@@ -411,22 +411,34 @@ class CookBook (object):
                     file_hash=recipe.get_checksum(), installed_files=[])
         return self.status[recipe_name]
 
+    def _recipe_is_reset(self, recipe_name):
+        if not recipe_name in self.status:
+            return True
+
+        st = self.status[recipe_name]
+        return not st.built_version and st.needs_build
+
     def _reset_recipe_status(self, recipe, change_name, previous, now):
+        if self._recipe_is_reset(recipe.name):
+            return
+
         m.message('Resetting {}\'s status because its {} changed from {} to {}'.format(
             recipe.name, change_name, previous, now))
         self.reset_recipe_status(recipe.name)
         if recipe.library_type == LibraryType.STATIC:
             rdeps = self.list_recipe_reverse_deps(recipe.name)
-            rdeps_names = ' '.join([r.name for r in rdeps])
-            m.message('Resetting also the status of all its reverse dependencies '
-                'because it\'s a static library: {}'.format(rdeps_names))
-            for r in rdeps:
-                self.reset_recipe_status(r.name)
+            rdeps = [r for r in rdeps if not self._recipe_is_reset(r.name)]
+            if rdeps:
+                rdeps_names = ' '.join([r.name for r in rdeps])
+                m.message('Resetting also the status of all its reverse dependencies '
+                    'because it\'s a static library: {}'.format(rdeps_names))
+                for r in rdeps:
+                    self.reset_recipe_status(r.name)
 
     def _reset_recipe_if_needed(self, recipe):
         # Check that the recipe still exists in cache, because
         # a prior recipe might have reset its status
-        if not recipe.name in self.status:
+        if self._recipe_is_reset(recipe.name):
             return
         st = self.status[recipe.name]
         # Need to check the version too, because the version can be
@@ -467,7 +479,8 @@ class CookBook (object):
             # allow safe relocation of the recipes.
             if recipe.__file__ != st.filepath:
                 st.filepath = recipe.__file__
-            recipe.run_func_depending_on_built_version(async_tasks, self._reset_recipe_if_needed, recipe)
+            if not self._recipe_is_reset(recipe.name):
+                recipe.run_func_depending_on_built_version(async_tasks, self._reset_recipe_if_needed, recipe)
 
         shell.run_until_complete(async_tasks)
 