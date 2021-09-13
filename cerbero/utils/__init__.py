bcfa8d6f5593a62249fc89f733f8850267bfb388
David Fernández <dfernandez@fluendo.com>
2021-09-13 18:06:34 +0200

    Add support for macOS Monterey

--- cerbero/utils/__init__.py
+++ cerbero/utils/__init__.py
@@ -347,7 +347,9 @@ Terminating.''', file=sys.stderr)
     elif platform == Platform.DARWIN:
         distro = Distro.OS_X
         ver = pplatform.mac_ver()[0]
-        if ver.startswith('10.16') or ver.startswith('11.4'):
+        if ver.startswith('12.'):
+            distro_version = DistroVersion.OS_X_MONTEREY
+        elif ver.startswith('10.16') or ver.startswith('11.'):
             distro_version = DistroVersion.OS_X_BIG_SUR
         elif ver.startswith('10.15'):
             distro_version = DistroVersion.OS_X_CATALINA
