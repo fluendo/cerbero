4b05e5ee3f713e17f1ab1351e1309c4f049fc0fd
Fabian Orccon <cfoch.fabian@gmail.com>
2022-05-12 21:16:54 +0200

    source: Fetch Git recipes in case of buildone
    The buildone command sets Oven.force to True and the Oven object
    sets Oven.recipe to Oven.force value.
    
    In the Git class, if Git.force is True, then the git repository
    should be always fetched.
    
    Issue: OCP_3227

--- cerbero/build/source.py
+++ cerbero/build/source.py
@@ -442,7 +442,7 @@ class Git (GitCache):
             # If we used cookbook.step_done, we'd be creating a new status
             # for those recipes that don't exist yet, introducing overhead.
             status = self.config.cookbook.status.get(recipe.name)
-            if status and 'fetch' in status.steps and not recipe.force:
+            if status and 'fetch' in status.steps and not self.force:
                 return True
         return False
 
