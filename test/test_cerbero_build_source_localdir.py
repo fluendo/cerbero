967124a83b6c6ba0b31ae0250f9559ff91f7cd06
Jorge Zapata <jorgeluis.zapata@gmail.com>
2024-06-03 18:41:21 +0200

    source: Add a LocalDir Source type
    This will allow in-place projects without the need to actually
    fetch the sources. Now projects can be delivered with a cerbero
    recipe as part of the distfiles if required. Also, simplifies
    testing by having local projects and recipes.

---
+++ test/test_cerbero_build_source_localdir.py
@@ -0,0 +1,58 @@
+# cerbero - a multi-platform build system for Open Source software
+# Copyright (C) 2012 Andoni Morales Alastruey <ylatuya@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+import unittest
+import os
+import tempfile
+import shutil
+
+from test.test_common import DummyConfig
+from cerbero.build.cookbook import CookBook
+from cerbero.build.oven import Oven
+from cerbero.utils import run_until_complete
+
+
+class LocalDirTest(unittest.TestCase):
+    def setUp(self):
+        self.config = DummyConfig()
+        self.tmp = tempfile.mkdtemp()
+        self.config.prefix = self.tmp
+        self.config.external_recipes = {'test': (os.path.join(os.path.dirname(__file__), 'recipes'), 1)}
+        self.cookbook = CookBook(self.config)
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp)
+
+    def LocalDirProject(self, name, file):
+        # Build the autotools project
+        recipe_name = self.cookbook.get_closest_recipe(name)
+        recipe = self.cookbook.get_recipe(recipe_name)
+        oven = Oven([recipe_name], self.cookbook, force=True)
+        run_until_complete(oven.start_cooking())
+        files = recipe.files_list()
+        # Check for the existance of file1.txt
+        self.assertEqual(files, [file])
+
+    def testLocalDirAutotools(self):
+        self.LocalDirProject('autotools-cerbero', 'share/autotools-cerbero/file1.txt')
+
+    def testLocalDirMeson(self):
+        self.LocalDirProject('meson-cerbero', 'share/meson-cerbero/file1.txt')
+
+    def testLocalDirCMake(self):
+        self.LocalDirProject('cmake-cerbero', 'share/cmake-cerbero/file1.txt')
