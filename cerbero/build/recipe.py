d4913aa3c0593c94702222288e4336641581cbff
Jorge Zapata <jorgeluis.zapata@gmail.com>
2024-06-03 18:23:00 +0200

    recipe: repo_dir doesn't belong here
    The code was a safe path to avoid sources access a not set property
    This property belongs to the GitCache source itself. In fact, it was
    overwritten there with a different value.

--- cerbero/build/recipe.py
+++ cerbero/build/recipe.py
@@ -281,10 +281,7 @@ SOFTWARE LICENSE COMPLIANCE.\n\n"""
     def __init__(self, config, env):
         self.config = config
         if self.package_name is None:
-            self.package_name = '%s-%s' % (self.name, self.version)
-        if not hasattr(self, 'repo_dir'):
-            self.repo_dir = os.path.join(self.config.local_sources, self.package_name)
-        self.repo_dir = os.path.abspath(self.repo_dir)
+            self.package_name = "%s-%s" % (self.name, self.version)
         self.build_dir = os.path.abspath(os.path.join(self.config.sources, self.package_name))
         self.config_src_dir = self.build_dir
         self.deps = self.deps or []
