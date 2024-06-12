8c9a1e9ecc33a29e07f9929e8917e4a5d7690eaa
Andoni Morales Alastruey <amorales@fluendo.com>
2024-06-12 16:36:32 +0200

    meson: adjust system for emscripten builds

--- cerbero/build/build.py
+++ cerbero/build/build.py
@@ -1080,6 +1080,10 @@ class Meson(Build, ModifyEnvBase):
         for k, v in binaries.items():
             extra_binaries += '{} = {}\n'.format(k, str(v))
 
+        system = self.config.target_platform
+        if self.config.target_distro == Distro.EMSCRIPTEN:
+            system = 'emscripten'
+
         contents = MESON_FILE_TPL.format(
             system=system,
             cpu=self.config.target_arch,
