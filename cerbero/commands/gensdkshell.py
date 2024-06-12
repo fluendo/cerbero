dac18dc6a17ac11807f541ce8496b2fd970c0dd2
Andoni Morales Alastruey <amorales@fluendo.com>
2024-06-12 21:04:26 +0200

    env: add gstreamer plugins path to PKG_CONFIG_PATH

--- cerbero/commands/gensdkshell.py
+++ cerbero/commands/gensdkshell.py
@@ -96,7 +96,8 @@ class GenSdkShell(Command):
         self._putvar('LD_LIBRARY_PATH', '%s${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}' % libdir)
         self._putvar(
             'PKG_CONFIG_PATH',
-            '%s/pkgconfig:%s/share/pkgconfig' '${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}' % (libdir, prefix_env),
+            f'{libdir}/pkgconfig:{libdir}/gstreamer-1.0/pkgconfig:{prefix_env}/share/pkgconfig'
+            + '${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}',
         )
         self._putvar('XDG_DATA_DIRS', '%s/share${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}' % prefix_env)
         self._putvar('XDG_CONFIG_DIRS', '%s/etc/xdg${XDG_CONFIG_DIRS:+:$XDG_CONFIG_DIRS}' % prefix_env)
